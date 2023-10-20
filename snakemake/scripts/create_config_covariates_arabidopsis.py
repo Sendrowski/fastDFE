9f3546c51045779d00102b518d015f1deda06dc9
Sendrowski <j.sendrowski18@gmail.com>
2023-10-20 10:16:41 +0200

    Update file.

--- snakemake/scripts/create_config_covariates_arabidopsis.py
+++ snakemake/scripts/create_config_covariates_arabidopsis.py
@@ -13,65 +13,83 @@ try:
 
     # necessary to import fastdfe locally
     sys.path.append('..')
+
     testing = False
     sfs_file = snakemake.input[0]
     col = snakemake.params.col
+    n_bins = snakemake.params.n_bins
     param = snakemake.params.param
     out = snakemake.output[0]
 except NameError:
     # testing
     testing = True
     sfs_file = "results/sfs_covariates/arabidopsis.csv"
-    col = 'age.rsa'
+    col = 'mean.rsa'
+    n_bins = 10
     param = 'S_d'
     out = "scratch/sfs_arabidopsis.yaml"
 
 import fastdfe as fd
 
+# read SFS stratified by gene
 genes = pd.read_csv(sfs_file, sep="\t")
 
-genes['group'] = pd.cut(
+# create bins
+genes['group'] = pd.qcut(
     x=genes[col],
-    bins=[0, 1, 2, 7, 10, 11, 13, 18],
-    labels=['0-1', '1-2', '2-4', '4-10', '10-11', '11-13', '13-18']
+    q=n_bins,
+    labels=[f"bin_{i}" for i in range(1, n_bins + 1)],
 )
 
-grouped = genes.groupby('group', observed=True).sum()
+# group by bins
+grouped = genes.groupby('group', observed=True)
 
+# extract SFS
 cols_neut = ['Lps'] + [f'sfsS{i}' for i in range(1, 105)] + ['Ds']
 cols_sel = ['Lpn'] + [f'sfsN{i}' for i in range(1, 105)] + ['Dn']
 
+# create spectra objects
 sfs_neut = fd.Spectra.from_dict(
-    {'.'.join([col, k]): r.values for k, r in grouped[cols_neut].iterrows()}
-).replace_dots()
+    {k: r.values for k, r in grouped.sum()[cols_neut].iterrows()}
+)
 
 sfs_sel = fd.Spectra.from_dict(
-    {'.'.join([col, k]): r.values for k, r in grouped[cols_sel].iterrows()}
-).replace_dots()
+    {k: r.values for k, r in grouped.sum()[cols_sel].iterrows()}
+)
 
+# determine average value of covariate in each bin
+cov = grouped[col].mean()
+
+# shuffle covariate values for testing
+# cov = dict(zip(cov.index, np.random.permutation(cov.values)))
+
+# create covariate object
+covariates = fd.Covariate(
+    param=param,
+    values=dict((t, cov[t]) for t in sfs_neut.types)
+)
+
+# create config object
 config = fd.Config(
     sfs_neut=sfs_neut.subsample(20),
     sfs_sel=sfs_sel.subsample(20),
     shared_params=[fd.SharedParams(params='all', types='all')],
-    #covariates=[fd.Covariate(
-    #    param=param,
-    #    values=dict((t, int(t.split('_')[-1])) for t in sfs_neut.types)
-    #)],
+    covariates=[covariates],
     n_runs=10,
     n_bootstraps=100,
     do_bootstrap=True,
     parallelize=True,
-    seed=1234,
+    seed=1234
 )
 
-# Spectra.from_spectra(dict(neutral=sfs_neut.all, selected=sfs_sel.all)).plot(show=testing)
+# plot SFS
+if testing:
+    (config.data['sfs_neut'].prefix('neutral') + config.data['sfs_sel'].prefix('selected')).plot(
+        show=testing,
+        use_subplots=True
+    )
 
+# save config
 config.to_file(out)
-
-inf = fd.JointInference.from_config(config)
-
-inf.run()
-
-inf.plot_discretized()
 
 pass
